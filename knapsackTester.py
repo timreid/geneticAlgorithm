b2e56513c067082e8e6d3f96e03d6228a48bfb2e
tim reid <timreid@gmail.com>
2018-03-10 15:51:32 -0800

    minor cleanup

--- knapsackTester.py
+++ knapsackTester.py
@@ -6,57 +6,57 @@ import numpy
 from geneticAlgorithm import *
 
 def makeKnapsackGenome(knapsackProblemSize):
-    """
-    makes a random knapsack genome
-    """
-    genome = []
-    for _ in range(knapsackProblemSize):
-        if numpy.random.uniform(0.0, 1.0) <= 1/knapsackProblemSize:
-            genome.append(True)
-        else:
-            genome.append(False)
-    return genome
+	"""
+	makes a random knapsack genome
+	"""
+	genome = []
+	for _ in range(knapsackProblemSize):
+		if numpy.random.uniform(0.0, 1.0) <= 1/knapsackProblemSize:
+			genome.append(True)
+		else:
+			genome.append(False)
+	return genome
 
 
 def knapsackGenomeWriter(genome):
-    """
-    pretty prints a knapsack genome
-    """
-    output = ""
-    for codon in genome:
-        if codon: output += "1"
-        else: output += "0"
-    return output
+	"""
+	pretty prints a knapsack genome
+	"""
+	output = ""
+	for codon in genome:
+		if codon: output += "1"
+		else: output += "0"
+	return output
 
 def makeKnapsackProblem(nItems, weightSigma, valueSigma):
-    """
-    makes a random instance of the knapsack problem of the form:
-    knapsackProblem = [(weight, value)]
-    Args:
-        nItems: the number of items
-        weightSigma: the sigma for the distribution of item weights
-        valueSigma: the sigma for the distribution of the item values
-    """
-    knapsackProblem = []
-    for _ in range(nItems):
-        weight = abs(numpy.random.normal(0.0, weightSigma))
-        value = abs(numpy.random.normal(0.0, valueSigma))
-        knapsackProblem.append((weight, value))
-    return knapsackProblem
-    
+	"""
+	makes a random instance of the knapsack problem of the form:
+	knapsackProblem = [(weight, value)]
+	Args:
+		nItems: the number of items
+		weightSigma: the sigma for the distribution of item weights
+		valueSigma: the sigma for the distribution of the item values
+	"""
+	knapsackProblem = []
+	for _ in range(nItems):
+		weight = abs(numpy.random.normal(0.0, weightSigma))
+		value = abs(numpy.random.normal(0.0, valueSigma))
+		knapsackProblem.append((weight, value))
+	return knapsackProblem
+	
 def makeKnapsackFitness(knapsackProblem, maxWeight):
-    """
-    returns a fitness function for a knapsack problem
-    """
-    def knapsackFitness(state, genome):
-        weight = 0.0
-        fitnessScore = 0.0
-        for i in range(len(genome)):
-            weight += genome[i] * knapsackProblem[i][0]
-            fitnessScore += genome[i] * knapsackProblem[i][1]
-        if weight > maxWeight: fitnessScore = 0.0
-        return fitnessScore
-    return knapsackFitness
+	"""
+	returns a fitness function for a knapsack problem
+	"""
+	def knapsackFitness(state, genome):
+		weight = 0.0
+		fitnessScore = 0.0
+		for i in range(len(genome)):
+			weight += genome[i] * knapsackProblem[i][0]
+			fitnessScore += genome[i] * knapsackProblem[i][1]
+		if weight > maxWeight: fitnessScore = 0.0
+		return fitnessScore
+	return knapsackFitness
 
 #construct a random knapsack problem
 knapsackProblemSize = 1000
@@ -71,9 +71,9 @@ initialState = {"mutationRate":0.05}
 populationSize = 100
 initialPopulation = []
 for _ in range(populationSize):
-    genome = makeKnapsackGenome(knapsackProblemSize)
-    fitnessScore = fitness(initialState, genome)
-    individual = Individual(fitnessScore, genome)
-    initialPopulation.append(individual)
+	genome = makeKnapsackGenome(knapsackProblemSize)
+	fitnessScore = fitness(initialState, genome)
+	individual = Individual(fitnessScore, genome)
+	initialPopulation.append(individual)
 
 geneticAlgorithm(monitor, simpleSelect, fitness, simpleCrossover, simpleBooleanMutation, initialState, initialPopulation)
