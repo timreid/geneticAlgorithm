e3a2565d483a573fdd13dbe1b341d7b07a9ce614
tim reid <timreid@gmail.com>
2018-03-11 17:10:38 -0700

    refactor to better confirm to PEP8

---
+++ genetic_algorithm.py
@@ -0,0 +1,113 @@
+"""
+a simple yet flexible implementation of the classic genetic optimization algorithm.
+it operates by maintaining a list of solutions, known as a population, beginning with a
+supplied inital population which is repeatedly reproduced by selecting individuals for
+reproduction in a manner analagous to natural selection and recombining them into new
+solution in a manner analagous to genetic crossover.
+"""
+
+from collections import namedtuple
+import numpy
+
+#an individual is a representation of a solution to the problem, consisting of a genome
+#which is the solution itself, and its fitness score which is the quality of the solution.
+Individual = namedtuple("Individual", "fitness genome")
+
+def genetic_algorithm(monitor, select, fitness, crossover, mutation, initial_state, initial_population):
+    """
+    optimizes the supplied inital population by repeatedly generating new populations
+    until a goal condition is met.
+    Args:
+        monitor: logs progress and decides when a goal condition has been met.
+        select: chooses two parents for reproduction.
+        fitness: provides a score for a genome.
+        crossover: constructs a new genome from two parents genomes.
+        mutation: applies a slight change to a genome with some small probability.
+        initial_state: the starting state of the run.
+        initial_population: is the starting population of the run.
+    """
+
+    if not "generation" in initial_state:
+        initial_state["generation"] = 0
+    state = initial_state
+    population = initial_population
+
+    #repeatedly generate new populations until the monitor sets the state to False
+    while state:
+        #the best solution, known as the elite, is automatically carried into the next generation
+        #thereby making the optimization monotonic.
+        new_population = [population[0]]
+        #construct the next population by selecting parents from the current population and
+        #crossing them over to create a child
+        while len(new_population) < len(initial_population):
+            mom = select(state, population)
+            dad = select(state, population)
+            child_genome = crossover(state, mom.genome, dad.genome)
+            mutation(state, child_genome)
+            child_fitness = fitness(state, child_genome)
+            child = Individual(child_fitness, child_genome)
+            new_population.append(child)
+        #populations are sorted by descending fitness
+        new_population.sort(key=lambda individual: individual.fitness, reverse=True)
+        population = new_population
+        #monitor will perform any desired logging and set the state to False to indicate
+        #when a goal condition is reached.
+        monitor(state, population)
+
+def make_simple_monitor(output_filename, genome_writer):
+    """
+    returns a basic monitor which logs the generation number, the elite's fitness, and the elite's
+    pretty printed genome. it sets the state to False when either a maximum number of generations or
+    a sufficiently high elite fitness is reached.
+    Args:
+        output_filename: the name given to the log file.
+        genome_writer: the genome pretty printer.
+    """
+    log = open(output_filename, "w")
+    def monitor(state, population):
+        """
+        log each generation, and check for goal conditions
+        """
+        state["elite"] = population[0]
+        state["generation"] += 1
+        generation = state.get("generation")
+        elite = state.get("elite")
+        log.write(str(generation))
+        log.write("\t")
+        log.write(str(elite.fitness))
+        log.write("\t")
+        log.write(genome_writer(elite.genome))
+        log.write("\n")
+        log.flush()
+        max_generations = state.get("max_generations")
+        goal_fitness = state.get("goal_fitness")
+        #check if a goal condition has been met
+        if (max_generations and generation >= max_generations) or (goal_fitness and elite.fitness >= goal_fitness):
+            log.close()
+            #setting state to False signifies the end of the run
+            state = False
+    return monitor
+
+
+def simple_select(state, population):
+    """
+    selects an individual from the population using gaussian sampling.
+    """
+    sigma = len(population) / 3
+    #gaussian sampling with rejection
+    i = int(abs(numpy.random.normal(0.0, sigma)))
+    while i > len(population) - 1:
+        #sample was too big, reject it and try again
+        i = int(abs(numpy.random.normal(0.0, sigma)))
+    selected = population[i]
+    return selected
+
+def simple_crossover(state, mom_genome, dad_genome):
+    """
+    creates a child genome from two parents by cutting them at a random point and
+    taking one slice from each.
+    """
+    #assumption: genomes are same length
+    crossover_point = numpy.random.random_integers(0, len(mom_genome) - 1)
+    child_genome = mom_genome[0:crossover_point] + dad_genome[crossover_point:]
+    return child_genome
---
+++ knapsack_tester.py
@@ -0,0 +1,135 @@
+"""
+a simple test application for the genetic algorithm.
+the knapsack problem is to choose from a list of items, each with associated value and size,
+such that the value of the selection is maximized but does not exceed the size of the knapsack.
+"""
+import numpy
+
+from genetic_algorithm import genetic_algorithm, Individual, simple_crossover, simple_select
+
+def simple_boolean_mutation(state, genome):
+    """
+    randomize each codon in a genome with small probability
+    """
+    for codon in genome:
+        if numpy.random.uniform(0.0, 1.0) <= state.get("mutation_rate"):
+            codon = numpy.random.choice([True, False])
+
+def make_knapsack_monitor(output_filename, knapsack_problem):
+    """
+    returns a basic monitor which logs the generation number, the elite's fitness, and
+    the elite's pretty printed genome. it sets the state to False when either a
+    maximum number of generations or a sufficiently high elite fitness is reached.
+    Args:
+        output_filename: the name given to the log file.
+        knapsack_problem: the instance of the knapsack problem.
+    """
+    def knapsack_genome_writer(genome):
+        """
+        pretty prints a knapsack genome
+        """
+        output = ""
+        for codon in genome:
+            if codon:
+                output += "1"
+            else:
+                output += "0"
+        return output
+
+    def calculate_weight(genome):
+        """
+        calculates the total weight of items described in the genome
+        """
+        weight = 0.0
+        for i, _ in enumerate(genome):
+            weight += genome[i] * knapsack_problem[i][0]
+        return weight
+
+    log = open(output_filename, "w")
+    def monitor(state, population):
+        """
+        perform logging and check for goal condition
+        """
+        state["elite"] = population[0]
+        state["generation"] += 1
+        generation = state.get("generation")
+        elite = state.get("elite")
+        log.write(str(generation))
+        log.write("\t")
+        log.write(str(elite.fitness))
+        log.write("\t")
+        log.write(str(calculate_weight(elite.genome)))
+        log.write("\t")
+        log.write(knapsack_genome_writer(elite.genome))
+        log.write("\n")
+        log.flush()
+        max_generations = state.get("max_generations")
+        goal_fitness = state.get("goal_fitness")
+        #check if a goal condition has been met
+        if (max_generations and generation >= max_generations) or (goal_fitness and elite.fitness >= goal_fitness):
+            log.close()
+            state = False
+    return monitor
+
+def make_knapsack_genome(knapsack_problem_size):
+    """
+    makes a random knapsack genome
+    """
+    genome = []
+    for _ in range(knapsack_problem_size):
+        if numpy.random.uniform(0.0, 1.0) <= 1/knapsack_problem_size:
+            genome.append(True)
+        else:
+            genome.append(False)
+    return genome
+
+def make_knapsack_problem(n_items, weight_sigma, value_sigma):
+    """
+    makes a random instance of the knapsack problem of the form:
+    knapsack_problem = [(weight, value)]
+    Args:
+        n_items: the number of items
+        weight_sigma: the sigma for the distribution of item weights
+        value_sigma: the sigma for the distribution of the item values
+    """
+    knapsack_problem = []
+    for _ in range(n_items):
+        weight = abs(numpy.random.normal(0.0, weight_sigma))
+        value = abs(numpy.random.normal(0.0, value_sigma))
+        knapsack_problem.append((weight, value))
+    return knapsack_problem
+def make_knapsack_fitness(knapsack_problem, max_weight):
+    """
+    returns a fitness function for a knapsack problem
+    """
+    def knapsack_fitness(state, genome):
+        """
+        calculates the fitness score of the genome
+        """
+        weight = 0.0
+        fitness_score = 0.0
+        for i, _ in enumerate(genome):
+            weight += genome[i] * knapsack_problem[i][0]
+            fitness_score += genome[i] * knapsack_problem[i][1]
+        if weight > max_weight:
+            fitness_score = 0.0
+        return fitness_score
+    return knapsack_fitness
+
+#construct a random knapsack problem
+KNAPSACK_PROBLEM_SIZE = 100
+KNAPSACK_PROBLEM = make_knapsack_problem(KNAPSACK_PROBLEM_SIZE, 100.0, 100.0)
+MAX_WEIGHT = 100
+MONITOR = make_knapsack_monitor("output", KNAPSACK_PROBLEM)
+FITNESS = make_knapsack_fitness(KNAPSACK_PROBLEM, MAX_WEIGHT)
+INITIAL_STATE = {"mutation_rate":0.05}
+#construct a random initial population
+POPULATION_SIZE = 100
+INITIAL_POPULATION = []
+for _ in range(POPULATION_SIZE):
+    current_genome = make_knapsack_genome(KNAPSACK_PROBLEM_SIZE)
+    current_fitness_score = FITNESS(INITIAL_STATE, current_genome)
+    individual = Individual(current_fitness_score, current_genome)
+    INITIAL_POPULATION.append(individual)
+
+genetic_algorithm(MONITOR, simple_select, FITNESS, simple_crossover, simple_boolean_mutation, INITIAL_STATE, INITIAL_POPULATION)
